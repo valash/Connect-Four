a8d9447dfdf3d2ec1a44033dca3af6f28b0bc84f
Virginia <virginialash1@gmail.com>
2019-02-04 16:45:18 -0500

    tried a bunch of things and got nothing

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const cells = document.querySelectorAll('.cell');
 const button = document.querySelectorAll("button");
 let currentPlayer = 1;
 
+    
+
+console.log(fullGrid);
+
+
+
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click',function(evt){
@@ -17,8 +23,43 @@ for (let i = 0; i < cells.length; i++) {
         this.style.pointerEvents="none";
 })
 }
+//https://levelup.gitconnected.com/building-a-tic-tac-toe-game-app-with-javascript-5916e58071fb
 
-
+// function checkForWinner() {
+//     console.log("checking for winner...")
+//     var winner
+//     var eightWinningCombos = [
+//       "#one.COLOR, #two.COLOR, #three.COLOR",
+//       "#four.COLOR, #five.COLOR, #six.COLOR",
+//       "#seven.COLOR, #eight.COLOR, #nine.COLOR",
+//       "#one.COLOR, #four.COLOR, #seven.COLOR",
+//       "#two.COLOR, #five.COLOR, #eight.COLOR",
+//       "#three.COLOR, #six.COLOR, #nine.COLOR",
+//       "#one.COLOR, #five.COLOR, #nine.COLOR",
+//       "#seven.COLOR, #five.COLOR, #three.COLOR"
+//     ]
+//     var blueWinArray = getWinningArray(eightWinningCombos, "darkslateblue")
+//     var yellowWinArray = getWinningArray(eightWinningCombos, "yellow")
+//     var blueWins = blueWinArray.includes(true)
+//     var yellowWins = redWinArray.includes(true)
+//     var fullGrid = getYellowCount() + getBlueCount()
+//     var draw = (fullGrid === 9) && (!blueWins) && (!yellowWins)
+    
+//     if (blueWins) {
+//       playerOneWins()
+//       return winner = blueWins
+//     }
+//     if (yellowWins) {
+//       computerWins()
+//       return winner = yelloWins
+//     }
+//     if (draw) {
+//       drawGame()
+//       return winner = draw
+//     } else {
+//       console.log('game on...')
+//     }
+//   }
 
 
 
